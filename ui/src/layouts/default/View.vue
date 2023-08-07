b7c5aa69a2ca7c0ef167e08a5970f02a4976493c
xyven1 <git@xyven.dev>
2023-08-07 13:58:33 -0400

    add refresh button if disconnected

--- ui/src/layouts/default/View.vue
+++ ui/src/layouts/default/View.vue
@@ -5,9 +5,14 @@
         <component :is="Component" />
       </KeepAlive>
     </RouterView>
-    <VSnackbar :model-value="!connected" location="top" color="error" :timeout="0" @update:model-value="() => {}">
+    <VSnackbar :model-value="!connected" location="top" color="error" :timeout="0" class="d-flex" @update:model-value="() => {}">
       <VIcon :icon="mdiLanDisconnect" />
       Disconnected
+      <template #actions>
+        <VBtn @click="refresh">
+          Refresh
+        </VBtn>
+      </template>
     </VSnackbar>
   </VMain>
 </template>
@@ -16,4 +21,5 @@ import { useAppStore } from '@/store/app';
 import { mdiLanDisconnect } from '@mdi/js';
 import { storeToRefs } from 'pinia';
 const { connected } = storeToRefs(useAppStore());
+const refresh = () => location.reload();
 </script>
