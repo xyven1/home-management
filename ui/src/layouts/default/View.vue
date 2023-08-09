b9517fde91253ddfb1e4ff01b618c903c2f37531
xyven1 <git@xyven.dev>
2023-08-08 22:18:53 -0400

    pwa needRefresh test

--- ui/src/layouts/default/View.vue
+++ ui/src/layouts/default/View.vue
@@ -14,12 +14,23 @@
         </VBtn>
       </template>
     </VSnackbar>
+    <VSnackbar :model-value="needRefresh" location="top" color="warning" :timeout="0" class="d-flex" @update:model-value="() => {}">
+      <VIcon :icon="mdiLanDisconnect" />
+      Please refresh
+      <template #actions>
+        <VBtn @click="refresh">
+          Refresh
+        </VBtn>
+      </template>
+    </VSnackbar>
   </VMain>
 </template>
 <script lang="ts" setup>
 import { useAppStore } from '@/store/app';
 import { mdiLanDisconnect } from '@mdi/js';
 import { storeToRefs } from 'pinia';
+import { useRegisterSW } from 'virtual:pwa-register/vue';
 const { connected } = storeToRefs(useAppStore());
+const { needRefresh } = useRegisterSW();
 const refresh = () => location.reload();
 </script>
